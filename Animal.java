b6876355c07294a29450758fc10e4c1af673205c
Andreu Monforte <59867802+Copyrighter22@users.noreply.github.com>
2021-11-26 17:19:40 +0100

    26/11/21 - 17:19 pm

--- Animal.java
+++ Animal.java
@@ -6,11 +6,18 @@ public class Animal {
     }
 
     boolean talking() {
+        String animalfinal = "";
+        String nomguardar = "";
+        char caracterfinal = ' ';
+        int cout = 0;
+        String obci;
         while (true) {
+
             System.out.println("bup bup");
             System.out.println("");
             byte pregunta;
             String Tipo;
+
 
             System.out.println("Selecciona quina pregunta vols:");
             System.out.println("1.Quin tipus d’animal ets?");
@@ -32,14 +39,21 @@ public class Animal {
                     char animal = ani.next().charAt(0);
                     int difG = 'g' - animal;
                     int difS = 's' - animal;
+
                     //Definim els condiconals en funcion a la posició numerica en valor Unicode dels caracters alfanumerics.
-                    if (difG >= -3 || animal <= 'g')
-                    System.out.println("Gos");
-                else if (difS <= -3 || animal >= 's')
-                    System.out.println("Serp");
-                else System.out.println("Mono");
+                    if (difG >= -3 || animal <= 'g') {
+                        System.out.println("Gos");
+                        animalfinal = "gos";}
+                    else if (difS <= -3 || animal >= 's') {
+                            System.out.println("Serp");
+                            animalfinal = "serp";
+                        } else {
+                            System.out.println("Mono");
+                            animalfinal = "gos";
+                        }
+
                     break;
-                    
+
                 case 2:
                     String VLetra;
                     System.out.println("A que t'agrada jugar?");
@@ -47,15 +61,25 @@ public class Animal {
                     VLetra = entradaLetra.nextLine();
                     if (VLetra.equals("A")) {
                         System.out.println("Atletisme");
-                    }else if (VLetra.equals("B")) {System.out.println("Boxeig");}
-                     else if (VLetra.equals("C")) {System.out.println("Ciclisme");}
-                     else if (VLetra.equals("D")) {System.out.println("Dansa");}
-                     else if (VLetra.equals("E")) {System.out.println("Esgrima");}
-                     else if (VLetra.equals("F")) {System.out.println("Futbol");}
-                     else if (VLetra.equals("G")) {System.out.println("Golf");}
-                     else if (VLetra.equals("H")) {System.out.println("Handbol");}
-                     else if (VLetra.equals("I")) {System.out.println("Indiaca");}
-                     else if (VLetra.equals("J")) {System.out.println("Judo");}
+                    } else if (VLetra.equals("B")) {
+                        System.out.println("Boxeig");
+                    } else if (VLetra.equals("C")) {
+                        System.out.println("Ciclisme");
+                    } else if (VLetra.equals("D")) {
+                        System.out.println("Dansa");
+                    } else if (VLetra.equals("E")) {
+                        System.out.println("Esgrima");
+                    } else if (VLetra.equals("F")) {
+                        System.out.println("Futbol");
+                    } else if (VLetra.equals("G")) {
+                        System.out.println("Golf");
+                    } else if (VLetra.equals("H")) {
+                        System.out.println("Handbol");
+                    } else if (VLetra.equals("I")) {
+                        System.out.println("Indiaca");
+                    } else if (VLetra.equals("J")) {
+                        System.out.println("Judo");
+                    }
 
                     break;
                 case 3:
@@ -65,14 +89,14 @@ public class Animal {
                     System.out.println("Quin es el teu caracter favorit?");
                     Scanner entradaCaracter = new Scanner(System.in);
                     Caracter = entradaCaracter.next().charAt(0);
-                    char contador = (char) ((Caracter-'A')/2);
-                    char CaracterFinal = (char) (contador + 'A');
+                    char contador = (char) ((Caracter - 'A') / 2);
+                    caracterfinal = (char) (contador + 'A');
 
-                    while (impresio != (char) (contador + 'A' )){
+                    while (impresio != (char) (contador + 'A')) {
                         System.out.print(impresio + ",");
                         ++impresio;
                     }
-                    System.out.printf(String.valueOf("el caracter que has elegit és " + CaracterFinal + ","));
+                    System.out.printf(String.valueOf("el caracter que has elegit és " + caracterfinal + ","));
                     break;
                 case 4:
                     break;
@@ -83,6 +107,7 @@ public class Animal {
 
                     int numloop = 0;
                     while (numloop != 10) {
+
                         char variable1 = ono.next().charAt(0);
                         System.out.println("TU:" + variable1);
                         char variable2 = (char) (r.nextInt(26) + 'a');
@@ -93,26 +118,38 @@ public class Animal {
                         String str = new StringBuilder().append(variable1).append(variable2).append(variable3).toString();
                         char vocal = str.charAt(1);
 
-                        if (vocal == 'a' || vocal == 'e' || vocal == 'i' || vocal == 'o' ||vocal == 'u')
+                        if (vocal == 'a' || vocal == 'e' || vocal == 'i' || vocal == 'o' || vocal == 'u'){
                             System.out.println("La combinació " + str + " es una onomatopeia");
-                        else
-                            System.out.println("La combinació " + str + " no es una onomatopeia");
+                        cout ++;}
+                        else {System.out.println("La combinació " + str + " no es una onomatopeia");
+                        cout ++;}
                     }
                     break;
                 case 6:
                     System.out.println("Dona'm un nom");
                     Scanner entradaNom = new Scanner(System.in);
-                    String NomGuardar = entradaNom.nextLine();
+                    nomguardar = entradaNom.nextLine();
                     System.out.println("Gràcies,m'agrada aquest nom.");
                     break;
                 case 7:
+                    System.out.println("El meu nom es nom "+ nomguardar +" soc un " + animalfinal + " el meu caràcter favorit es " + caracterfinal +" el total de onomatopeies es " +cout+ ".");
+
                     break;
                 case 8:
-                    return true;
 
+                    Scanner entrada8 = new Scanner(System.in);
+                    System.out.println("estas segur de que vols tancar?(digues si o no)");
+                    obci = entrada8.nextLine();
+                    if (obci.equals("si")) {
+                        return true;
+                    }
+                    if (obci.equals("no")) {
+                        break;
+
+                    }
             }
-            return true;
         }
-    }
 
+
+    }
 }
