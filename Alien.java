f920ff50c3e3546824400b7f4ea3e06d03c42f08
Aleix Montero <91148993+AleixMS5@users.noreply.github.com>
2021-11-26 17:18:48 +0100

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
 
 public class Alien {
-
     // Constructor
     void Alien() {
     }
 
     boolean talking() {
+        String nombre = "";
+        String tipus = "";
+        String vitage = "";
+        float favorit = 0;
+        float num1;
+        float opcio;
+        float num2;
+        float num;
+        float primer;
+        float segon;
+        String nom;
+        Scanner entrada = new Scanner(System.in);
+        String obcio;
         while (true) {
             System.out.println("1. Quin tipus d’extraterrestre ets? ");
             System.out.println("2. Com et desplaces? ");
@@ -18,26 +30,22 @@ public class Alien {
             System.out.println("6. Posa-li un nom");
             System.out.println("7. Pots recordar-me la nostra conversa? ");
             System.out.println("8. Sortir");
-            Scanner entrada = new Scanner(System.in);
-            float opcio;
-            float num1;
+
             opcio = entrada.nextFloat();
-            String nombre = "";
-            String tipus = "";
-            String vitage = "";
-            float favorit = 0;
+
             switch ((int) opcio) {
                 case 1:
+                    Scanner entrada1 = new Scanner(System.in);
                     System.out.println("Introduix un numero float de 0-10: ");
-                    num1 = entrada.nextFloat();
-                    if (num1 == 0.5 || num1 == 1.5 || num1 == 2.5 || num1 == 2.5 || num1 == 3.5 || num1 == 4.5 || num1 == 5.5 || num1 == 6.5 || num1 == 7.5 || num1 == 8.5 || num1 == 9.5) {
+                    num1 = entrada1.nextFloat();
+                    if (num1 == 0.5 || num1 == 1.5 || num1 == 2.5 || num1 == 3.5 || num1 == 4.5 || num1 == 5.5 || num1 == 6.5 || num1 == 7.5 || num1 == 8.5 || num1 == 9.5) {
                         System.out.println("Soc un alien,no m'has vist");
                         tipus = "Soc un alien,no m'has vist";
                     } else if ((num1 > 0.5 && num1 < 1.0) || (num1 > 1.5 && num1 < 2.0) || (num1 > 2.5 && num1 < 3.0) || (num1 > 3.5 && num1 < 4.0) || (num1 > 4.5 && num1 < 5.0) || (num1 > 5.5 && num1 < 6.0) || (num1 > 6.5 && num1 < 7.0) || (num1 > 7.5 && num1 < 8.0) || (num1 > 8.5 && num1 < 9.0) || (num1 > 9.5 && num1 < 10.0)) {
                         System.out.println("Soc un Alien de Pau");
                         tipus = "Soc un Alien de Pau";
                     } else System.out.println("Sou un Alien, et mataré ");
-                    tipus = "Sou un Alien, et mataré";
+                    tipus = "Soc un Alien, et mataré";
                     break;
                 case 2:
                     num1 = entrada.nextFloat();
@@ -74,8 +82,7 @@ public class Alien {
                     }
                     break;
                 case 3:
-                    float num2;
-                    float num;
+
                     num2 = entrada.nextFloat();
                     System.out.println("Quin és el teu número favorit entre 0,0 i ...?");
                     for (num = (float) 0; num < num2 / 2.0; num = (float) (num + 0.1)) {
@@ -83,16 +90,15 @@ public class Alien {
                         //System.out.println(num);
                     }
                     System.out.println("Has triat " + num2 / 2.0);
-                    favorit=num2;
+                    favorit = num2;
                     break;
 
                 case 4:
-                    float primer;
-                    float segon;
+
                     primer = entrada.nextFloat();
                     segon = entrada.nextFloat();
                     for (float i = primer; i <= (float) ((primer + segon) / 2.0); i = (float) (i + 0.1)) {
-                      if(i%2==1)System.out.printf("%2.1f ",i);
+                        if (i % 2 == 1) System.out.printf("%2.1f ", i);
                         System.out.println();
 //               for(float j = segon;j >= (float) ((primer + segon) / 2.0); j= (float) (j-0.1)){
 //                   System.out.printf("%2.1f ",j);
@@ -107,16 +113,24 @@ public class Alien {
 
                     break;
                 case 6:
-                    String nom;
-                    nom = entrada.nextLine();
-                    System.out.println("Gracies m'hagrada aquest nom" + nom);
+                    Scanner entrada6 = new Scanner(System.in);
+                    nom = entrada6.nextLine();
+                    System.out.println("Gracies m'hagrada aquest nom " + nom);
                     nombre = nom;
                     break;
                 case 7:
-                    System.out.println("Hem dic " + nombre + " i  soc un Alien " + tipus + ".  El meu solc  desplacant com " + vitage + " el meu número preferit es" + favorit + ".");
+                    System.out.println("Hem dic " + nombre + " i  " + tipus + ".  El meu solc  desplacant com " + vitage + " el meu número preferit es" + favorit + ".");
                     break;
-                default:
-                return true;
+                case 8:
+                    Scanner entrada8 = new Scanner(System.in);
+                    System.out.println("estas segur de que vols tancar?(digues si o no)");
+                    obcio = entrada8.nextLine();
+                    if (obcio.equals("si")) {
+                        return true;
+                    }
+                    if (obcio.equals("no")) {
+                        break;
+                    }
             }
         }
     }
